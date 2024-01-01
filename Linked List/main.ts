d11e6ceb486a941872059c5411a5b9d5799bdd09
DomaTomoharu <halimfathin7@gmail.com>
2024-01-01 18:33:52 +0700

    Improve

--- Linked List/main.ts
+++ Linked List/main.ts
@@ -6,17 +6,41 @@ class node {
   }
 }
 
-let node1 = new node("Node 1 Coy");
-let node2 = new node("Node 2 is the best");
-let node3 = new node("alamak node akhir");
-node1.next = node2;
-node2.next = node3;
-
-let nodeSaatIni: node | null = node1;
-
-while (nodeSaatIni !== null) {
-  console.log(
-    `node saat ini adalah "${nodeSaatIni.value}" dengan node selanjutnya ${nodeSaatIni.next?.value}`
-  );
-  nodeSaatIni = nodeSaatIni.next;
+function insertFirst(head, data) {
+  let noded = new node(data);
+  noded.next = head;
+  return noded;
 }
+
+function insertLast(head, data) {
+  let newNode = new node(data);
+
+  if (head === null) {
+    return newNode;
+  }
+
+  let current = head;
+  while (current.next !== null) {
+    current = current.next;
+  }
+
+  current.next = newNode;
+  return head;
+}
+
+function printList(head) {
+  console.log("Element Dalam List: ");
+  let current = head;
+  while (current !== null) {
+    console.log(current.value);
+    current = current.next;
+  }
+}
+
+let head: null | node = null;
+head = insertLast(head, "node 3");
+head = insertFirst(head, "node 2");
+head = insertLast(head, "node 4");
+head = insertLast(head, "node terakhir");
+head = insertFirst(head, "node 1");
+printList(head);
